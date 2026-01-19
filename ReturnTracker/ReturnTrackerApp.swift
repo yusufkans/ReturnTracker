ee3ef7b3cf394bd53be06219fb2c44c205fc64ac
Yusufkan Sürmelioğlu <yusufkan.surmelioglu@vestel.com.tr>
2026-01-19 12:14:03 +0300

    Initial Commit

---
+++ ReturnTracker/ReturnTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReturnTrackerApp.swift
+//  ReturnTracker
+//
+//  Created by Yusufkan Sürmelioğlu on 19.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct ReturnTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
